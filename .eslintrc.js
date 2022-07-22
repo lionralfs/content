fcdadc4094961d00da454a0708ced7f17f10a03f
Lion Ralfs <lion.ralfs@gmail.com>
2022-07-22 21:39:20 +0200

    use overrides

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   extends: [
     // "plugin:json/recommended"
-    "plugin:markdown/recommended",
   ],
+  plugins: ["markdown"],
   parserOptions: {
     ecmaVersion: "latest",
     // allows top-level await among other things
@@ -13,11 +13,28 @@ module.exports = {
     browser: true,
     node: true,
   },
-  rules: {
-    // "arrow-parens": "error",
-    // "no-var": "error",
-    // "prefer-const": "error",
-    "prefer-template": "error",
-    "template-curly-spacing": "error",
-  },
+  rules: {},
+  overrides: [
+    {
+      files: ["**/*.md"],
+      processor: "markdown/markdown",
+    },
+    {
+      // the markdown plugin assigns "virtual filenames" to fenced code blocks inside markdown,
+      // see: https://github.com/eslint/eslint-plugin-markdown#advanced-configuration
+      files: ["files/en-us/web/api/**/*.md/*.js"],
+      rules: {
+        "no-undef": "off",
+        "no-unused-expressions": "off",
+        "no-unused-vars": "off",
+        "padded-blocks": "off",
+        // "arrow-parens": "error",
+        // "no-var": "error",
+        // "prefer-const": "error",
+        // "prefer-template": "error",
+        // "template-curly-spacing": "error",
+        // "arrow-body-style": ["error", "as-needed"],
+      },
+    },
+  ],
 };
